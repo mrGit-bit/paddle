4832e78eedf035c22d5723e3cbcebe010f2ad50f
mrcorreoweb <145986471+mrcorreoweb@users.noreply.github.com>
2025-07-07 15:44:30 +0000

    feat(deploy): add PostgreSQL support for Supabase and update requirements for PostgreSQL and Railway deployment

--- paddle/config/settings/dev.py
+++ paddle/config/settings/dev.py
@@ -6,11 +6,9 @@ from .base import *
 from decouple import config, Csv
 
 SECRET_KEY = config("SECRET_KEY")
-
 DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv()) 
-
 CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", cast=Csv())
 
 # Database
@@ -32,7 +30,6 @@ STATICFILES_DIRS = [BASE_DIR / "frontend/static",] # Static path in development
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Local dev API URL
+# Local dev API URL & site URL
 BASE_API_URL = config("BASE_API_URL", default="http://127.0.0.1:8000/api/")
-
 SITE_URL = config("SITE_URL", default="http://127.0.0.1:8000/")
--- paddle/config/settings/prod.py
+++ paddle/config/settings/prod.py
@@ -1,33 +1,36 @@
-# path: paddle/config/settings/dev.py
-# settings for production
-# optimized for PythonAnywhere deployment
+# path: paddle/config/settings/prod.py
+# Production settings for Railway and Supabase PostgreSQL
 
 from .base import *
 from decouple import config, Csv
+import dj_database_url
 
 # Secret key in production settings
 # cSpell: disable-next-line
 SECRET_KEY = config("SECRET_KEY")
-
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default="glistening-solace.up.railway.app")
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", cast=Csv(), default="https://glistening-solace.up.railway.app")
 
-CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", cast=Csv())
+# # Mysql database and credentials
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": config("MYSQL_DB_NAME"),
+#         "USER": config("MYSQL_DB_USER"),
+#         "PASSWORD": config("MYSQL_DB_PASSWORD"),
+#         "HOST": config("MYSQL_DB_HOST", default="127.0.0.1"),
+#         "PORT": config("MYSQL_DB_PORT", default="3306"),
+#         "OPTIONS": {
+#             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
 
-# Mysql database and credentials
+# PostgreSQL database and credentials
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": config("MYSQL_DB_NAME"),
-        "USER": config("MYSQL_DB_USER"),
-        "PASSWORD": config("MYSQL_DB_PASSWORD"),
-        "HOST": config("MYSQL_DB_HOST", default="127.0.0.1"),
-        "PORT": config("MYSQL_DB_PORT", default="3306"),
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
+    "default": dj_database_url.config(default=config("DATABASE_URL"))
 }
 
 # serve static files from staticfiles directory
@@ -39,9 +42,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# change "yourdomain.com" to the actual pythonanywhere or server domain
-BASE_API_URL = config("BASE_API_URL", default="https://yourdomain.com/api/")
-SITE_URL = config("SITE_URL", default="https://yourdomain.com")
+# change "yourdomain.com" to the actual pythonanywhere or railway domain
+BASE_API_URL = config("BASE_API_URL", default="https://your-railway-app.up.railway.app/api/")
+SITE_URL = config("SITE_URL", default="https://your-railway-app.up.railway.app")
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
